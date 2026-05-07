70e2ed24c9a4d8b70b177be14b0a6f7fc0a59464
CiprianTiro <ciprian.tironeac@gmail.com>
2026-05-07 14:09:36 +0300

    Update asignment3 version

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 869e7abf7c1b254e7c324f958fe082c4aa025f68
+AESD_ASSIGNMENTS_VERSION = 37ca95edf068d6c427d1cf626751dbd2c61e1f95
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
